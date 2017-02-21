3c3f75bc6acff5fd4c96548d24972fb701fad1c2
Fiha <fihaas786@gmail.com>
2017-02-21 08:03:03 -0800

    made some more changes

--- anim.py
+++ anim.py
@@ -2,9 +2,11 @@ import turtle
 
 window=turtle.Screen()
 window.bgcolor("blue")
-for i in range(14,3,-1):
-	bro=turtle.Turtle()
-	bro.shape("turtle")
-	bro.color("yellow")
-	bro.circle(100-5*i,None,i)
+for i in range(8,2,-1):
+	for j in range(1,10):
+	
+		bro=turtle.Turtle()
+		bro.shape("turtle")
+		bro.color("red")
+		bro.circle(10*j,None,i)
 window.exitonclick()
